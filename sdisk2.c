90acad6cd291d2bf64e4c17457ebdfdbbe19675d
USHIRODA Atsushi <ushiroda.atsushi@nanja.info>
2016-11-23 13:45:26 +0900

    select swith

--- sdisk2.c
+++ sdisk2.c
@@ -8,6 +8,7 @@
 	Copyright 2013 Koichi NISHIDA
 	email to Koichi NISHIDA: tulip-house@msf.biglobe.ne.jp
 	
+	Modified by USHIRODA, Atsushi
 ------------------------------------------------------------*/
 
 /*
@@ -42,7 +43,8 @@ connection:
 	<others>
 	D5: LED
 	C5: connect to C6 (RESET) for software reset
-	D0: DOWN
+    D1: TxD
+	D0: RxD/DOWN
 	D5: UP
 	B0: ENTER
 	
@@ -62,7 +64,10 @@ connection:
 #define WAIT 1
 
 // write buffer number
-#define BUF_NUM 5
+#define BUF_NUM 5				// number of buffer
+#define BUF_SIZE 350			// size of a buffer
+#define OUT_TRACK 0xff			// out of track
+#define OUT_SECTOR OUT_TRACK  	// out of sector
 
 // fat buffer size
 #define FAT_DSK_SIZE 18
@@ -72,6 +77,16 @@ connection:
 #define SD_CS	2	// PB2 SS	CS
 
 // C prototypes
+
+#define IS_RESET bit_is_set(PIND,3)
+#define SOFT_RESET (PORTC &= (~0b00100000))
+#define SD_CS_HI (PORTB |= (1<<SD_CS))
+#define SD_CS_LO (PORTB &= ~(1<<SD_CS))
+#define LED_ON (PORTD |= 0b00100000)
+#define LED_OFF (PORTD &= ~(0b00100000))
+#define LED_FLASH (PORTD ^= 0b00100000)
+#define DISK2_WP_ON (PORTD |= 0b00010000) 
+#define DISK2_WP_OFF (PORTD &= ~0b00010000)
 
 // ===== low level SD card functions =====
 
@@ -103,8 +118,6 @@ int findExt(char *targExt, unsigned char *protect,
 // prepare the FAT table on memory
 void prepareFat(int i, unsigned short *fat, unsigned short len,
 	unsigned char fatNum, unsigned char fatElemNum);
-// memory copy	
-void memcp(unsigned char *dst, unsigned char *src, const unsigned short len);
 // duplicate FAT for FAT16
 void duplicateFat(void);
 // write to the SD cart one by one
@@ -165,11 +178,12 @@ const unsigned char volume = 0xfe;
 #define EEP_PH_TRACK (uint8_t *)0x0001
 
 // write data buffer
-unsigned char writeData[BUF_NUM][350];
-unsigned char sectors[BUF_NUM], tracks[BUF_NUM];
-unsigned char buffNum;
-unsigned char *writePtr;
-unsigned char doBuffering;
+unsigned char writeData[BUF_NUM][BUF_SIZE];			// write buffer
+unsigned char sectors[BUF_NUM];						// number of sector
+unsigned char tracks[BUF_NUM];						// number of track
+unsigned char buffNum;								// current buffer number
+unsigned char *writePtr;							// write data pointer
+unsigned char doBuffering;							// buffering flag
 
 // a table for head stepper moter movement 
 PROGMEM prog_uchar stepper_table[4] = {0x0f,0xed,0x03,0x21};
@@ -206,25 +220,30 @@ PROGMEM prog_uchar decTable[] = {
 
 // a table for translating logical sectors into physical sectors
 PROGMEM prog_uchar physicalSector[] = {
-		0,13,11,9,7,5,3,1,14,12,10,8,6,4,2,15};
+	0,13,11,9,7,5,3,1,14,12,10,8,6,4,2,15
+};
 
 // for bit flip
-PROGMEM prog_uchar FlipBit[] = { 0,  2,  1,  3  };
-PROGMEM prog_uchar FlipBit1[] = { 0, 2,  1,  3  };
-PROGMEM prog_uchar FlipBit2[] = { 0, 8,  4,  12 };
+PROGMEM prog_uchar FlipBit[]  = { 0,  2,  1,  3 };
+PROGMEM prog_uchar FlipBit1[] = { 0,  2,  1,  3 };
+PROGMEM prog_uchar FlipBit2[] = { 0,  8,  4, 12 };
 PROGMEM prog_uchar FlipBit3[] = { 0, 32, 16, 48 };
 
 // buffer clear
 void buffClear(void)
 {
-	unsigned char i;
-	unsigned short j;
+	int i;
+	int j;
 	
-	for (i=0; i<BUF_NUM; i++)
-		for (j=0; j<350; j++)
-			writeData[i][j]=0;
-	for (i=0; i<BUF_NUM; i++)
-		sectors[i]=tracks[i]=0xff;
+	for (i = 0; i < BUF_NUM; i++) {
+		for (j = 0; j < BUF_SIZE; j++) {
+			writeData[i][j] = 0;
+		}
+	}
+	for (i = 0; i < BUF_NUM; i++) {
+		sectors[i] = OUT_SECTOR;
+		tracks[i] = OUT_TRACK;
+	}
 }
 
 // cancel read from the SD card
@@ -247,7 +266,7 @@ void writeByte(unsigned char c)
 	SPDR = c;
 	// Wait for transmission complete
 	while (!(SPSR & (1<<SPIF)))
-		if (bit_is_set(PIND,3)) PORTC &= (~0b00100000);// if , reset
+		if (IS_RESET) SOFT_RESET;
 }
 
 // read data from the SD card
@@ -256,7 +275,7 @@ unsigned char readByte(void)
 	SPDR = 0xFF;
 	// Wait for reception complete
 	while (!(SPSR & (1<<SPIF)))
-		if (bit_is_set(PIND,3)) PORTC &= (~0b00100000);// if eject, reset
+		if (IS_RESET) SOFT_RESET;
 	// Return Data Register
 	return SPDR;
 }
@@ -274,11 +293,11 @@ void waitFinish(void)
 void cmd_(unsigned char cmd, unsigned long adr)
 {
 	writeByte(0xff);
-	writeByte(0x40+cmd);
-	writeByte(adr>>24);
-	writeByte((adr>>16)&0xff);
-	writeByte((adr>>8)&0xff);
-	writeByte(adr&0xff);
+	writeByte(0x40 + cmd);
+	writeByte(adr >> 24);
+	writeByte((adr >> 16) & 0xff);
+	writeByte((adr >> 8) & 0xff);
+	writeByte(adr & 0xff);
 	writeByte(0x95);
 	writeByte(0xff);
 }
@@ -288,14 +307,7 @@ void cmd(unsigned char cmd, unsigned long adr)
 {
 	unsigned char res;
 	do {
-		writeByte(0xff);
-		writeByte(0x40+cmd);
-		writeByte(adr>>24);
-		writeByte((adr>>16)&0xff);
-		writeByte((adr>>8)&0xff);
-		writeByte(adr&0xff);
-		writeByte(0x95);
-		writeByte(0xff);
+		cmd_(cmd, adr);
 	} while (((res=getResp())!=0) && (res!=0xff));
 }
 
@@ -422,13 +434,6 @@ void prepareFat(int i, unsigned short *fat, unsigned short len,
 	cmd(16, (unsigned long)512);	
 }
 
-// memory copy
-void memcp(unsigned char *dst, unsigned char *src, unsigned short len)
-{
-	unsigned short i;
-	
-	for (i=0; i<len; i++) dst[i]=src[i];
-}
 
 void writeSD(unsigned long adr, unsigned char *data, unsigned short len)
 {
@@ -439,10 +444,10 @@ void writeSD(unsigned long adr, unsigned char *data, unsigned short len)
 	cmd17(adr&0xfffffe00);
 	for (i=0; i<512; i++) buf[i] = readByte();
 	readByte(); readByte(); // discard CRC bytes
-	memcp(&(buf[adr&0x1ff]), data, len);
+	memcpy(&(buf[adr&0x1ff]), data, len);
 	
-	PORTB |= (1<<SD_CS);
-	PORTB &= ~(1<<SD_CS);
+	SD_CS_HI;
+	SD_CS_LO;
 				
 	cmd(24,adr&0xfffffe00);		
 	writeByte(0xff);
@@ -453,8 +458,8 @@ void writeSD(unsigned long adr, unsigned char *data, unsigned short len)
 	readByte();
 	waitFinish();
 	
-	PORTB |= (1<<SD_CS);
-	PORTB &= ~(1<<SD_CS);	
+	SD_CS_HI;
+	SD_CS_LO;	
 }
 
 void duplicateFat(void)
@@ -469,8 +474,8 @@ void duplicateFat(void)
 		for (i=0; i<512; i++) buf[i] = readByte();
 		readByte(); readByte(); // discard CRC bytes
 
-		PORTB |= (1<<SD_CS);
-		PORTB &= ~(1<<SD_CS);	
+		SD_CS_HI;
+		SD_CS_LO;	
 	
 		cmd(24,adr+(unsigned long)sectorsPerFat*512);		
 		writeByte(0xff);
@@ -482,8 +487,8 @@ void duplicateFat(void)
 		waitFinish();
 		adr += 512;
 
-		PORTB |= (1<<SD_CS);
-		PORTB &= ~(1<<SD_CS);
+		SD_CS_HI;
+		SD_CS_LO;
 	}
 }
 
@@ -499,8 +504,8 @@ int createFile(unsigned char *name, char *ext, unsigned short sectNum)
 	if (bit_is_set(PIND,3)) return 0;
 	
 	for (i=0; i<32; i++) dirEntry[i]=0;
-	memcp(dirEntry, (unsigned char *)name, 8);
-	memcp(dirEntry+8, (unsigned char*)ext, 3);
+	memcpy(dirEntry, (unsigned char *)name, 8);
+	memcpy(dirEntry+8, (unsigned char*)ext, 3);
 	*(unsigned long *)(dirEntry+28) = (unsigned long)sectNum*512;
 	
 	// search a root directory entry
@@ -548,8 +553,8 @@ int createNic(unsigned char *name)
 	static unsigned char last[2] = {0xff, 0xff};
 	
 	for (i=0; i<32; i++) dirEntry[i]=0;
-	memcp(dirEntry, name, 8);
-	memcp(dirEntry+8, (unsigned char *)"NIC", 3);
+	memcpy(dirEntry, name, 8);
+	memcpy(dirEntry+8, (unsigned char *)"NIC", 3);
 	*(unsigned long *)(dirEntry+28) = (unsigned long)286720;
 	
 	// search a root directory entry
@@ -596,7 +601,7 @@ void dsk2Nic(void)
 	unsigned char *dst = (&writeData[0][0]+512);
 	unsigned short *fatDsk = (unsigned short *)(&writeData[0][0]+1024);
 
-	PORTD |= 0b00100000;
+	LED_ON;
 
 	prevFatNumNic = prevFatNumDsk = 0xff;
 
@@ -639,7 +644,7 @@ void dsk2Nic(void)
 
 	cmd(16, (unsigned long)512);	
 	for (trk = 0; trk < 35; trk++) {
-		PORTD ^= 0b00100000;
+		LED_FLASH;
 		for (logic_sector = 0; logic_sector < 16; logic_sector++) {
 			unsigned char *src;
 			unsigned short ph_sector = (unsigned short)pgm_read_byte_near(physicalSector+logic_sector);
@@ -705,8 +710,8 @@ void dsk2Nic(void)
 				}
 				ft = fatNic[long_cluster%FAT_NIC_SIZE];
 
-				PORTB |= (1<<SD_CS);
-				PORTB &= ~(1<<SD_CS);	
+				SD_CS_HI;
+				SD_CS_LO;	
 
 				cmd(24, userAddr+(((unsigned long)(ft-2)<<sectorsPerCluster2)
 					+ (long_sector&(sectorsPerCluster-1)))*(unsigned long)512);
@@ -719,13 +724,13 @@ void dsk2Nic(void)
 				readByte();
 				waitFinish();
 
-				PORTB |= (1<<SD_CS);
-				PORTB &= ~(1<<SD_CS);	
+				SD_CS_HI;
+				SD_CS_LO;	
 			}
 		}
 	}
 	buffClear();
-	PORTD &= ~(0b00100000);
+	LED_OFF;
 }
 
 // make file name list and sort
@@ -850,14 +855,14 @@ int SDinit(void)
 	unsigned char filebase[8], btfbase[8];
 	unsigned char btfExists, choosen;
 
-	PORTD |= 0b00100000;		// LED on
+	LED_ON;
 
 	//SPI enable, master mode, f/128
 	SPCR = ((1<<SPE)|(1<<MSTR)|(1<<SPR1)|(1<<SPR0));
 
-	PORTB |= (1<<SD_CS);					// disable CS
+	SD_CS_HI;					// disable CS
 	for(i=0; i < 10; i++) writeByte(0xFF);	// Send 10 * 8 = 80 clock
-	PORTB &= ~(1<<SD_CS);					// enable CS
+	SD_CS_LO;					// enable CS
 	for(i=0; i < 2; i++) writeByte(0xFF);	// Send 2 * 8 = 16 clock
 
 	cmd_(0, 0);	// command 0
@@ -865,21 +870,21 @@ int SDinit(void)
 		ch = readByte();
 	} while (ch != 0x01);
 	
-	PORTB |= (1<<SD_CS);		// disable CS
+	SD_CS_HI;		// disable CS
 
 	while (1) {
-		PORTB &= ~(1<<SD_CS);
+		SD_CS_LO;
 		cmd_(55, 0);			// command 55
 		ch = getResp();
 		if (ch == 0xff) return 0;
 		if (ch & 0xfe) continue;
 		// if (ch == 0x00) break;
-		PORTB |= (1<<SD_CS);
-		PORTB &= ~(1<<SD_CS);
+		SD_CS_HI;
+		SD_CS_LO;
 		cmd_(41, 0);			// command 41	
 		if (!(ch=getResp())) break;
 		if (ch == 0xff) return 0;
-		PORTB |= (1<<SD_CS);
+		SD_CS_HI;
 	}
 
 	//SPI enable, master mode, f/2
@@ -963,7 +968,7 @@ int SDinit(void)
 	prevFatNumDsk = 0xff;
 	cmd(16, (unsigned long)512);
 	SPCR = 0;					// disable spi
-	PORTD &= ~(0b00100000);	// LED off
+	LED_OFF;
 	
 	return 1;
 }
@@ -1062,13 +1067,13 @@ int main(void)
 	dispStr(LCD_ROW1, " DISK INSERTED  ");
 	while (1) {
 		if (bit_is_set(PIND, 7)) { 				// disable drive
-			PORTD &= ~(0b00100000);				// LED off
+			LED_OFF;
 		} else { 									// enable drive
-			PORTD |= 0b00100000;					// LED on
+			LED_ON;
 			if (bit_is_set(PIND,6)||protect||(PINC&2))
-				PORTD |= 0b00010000;
+				DISK2_WP_ON;
 			else
-				PORTD &= ~0b00010000;
+				DISK2_WP_OFF;
 			if (prepare) {
 				TIMSK0 &= ~(1<<OCIE0A);			// disable timer0
 				sector = ((sector+1)&0xf);
@@ -1124,8 +1129,8 @@ void writeBackSub(unsigned char bn, unsigned char sc, unsigned char track)
 	}
 	ft = fatNic[long_cluster%FAT_NIC_SIZE];
 	
-	PORTB |= (1<<SD_CS);
-	PORTB &= ~(1<<SD_CS);
+	SD_CS_HI;
+	SD_CS_LO;
 
 	cmd(24, (unsigned long)userAddr+(((unsigned long)(ft-2)<<sectorsPerCluster2)
 		+ (unsigned long)(long_sector&(sectorsPerCluster-1)))*512);
@@ -1192,8 +1197,8 @@ void writeBackSub(unsigned char bn, unsigned char sc, unsigned char track)
 	readByte();
 	waitFinish();
 
-	PORTB |= (1<<SD_CS);
-	PORTB &= ~(1<<SD_CS);	
+	SD_CS_HI;
+	SD_CS_LO;	
 
 	SPCR = 0;							// disable spi
 	TIMSK0 |= (1<<OCIE0A);				// enable timer0
@@ -1269,7 +1274,7 @@ ISR(INT1_vect)
 	eeprom_write_byte (EEP_PH_TRACK, ph_track); 
 
 	// and reset!
-	PORTC &= (~0b00100000);
+	SOFT_RESET;
 }
 	
 	
